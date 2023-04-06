652c0fa854351ccb61e9be641d5a5bffaf74f952
Wim Fournier <github@fournier.nl>
2023-04-06 22:31:35 +0200

    add parameters to binary

--- app/winterhill-web/main.go
+++ app/winterhill-web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
 	commandapi "github.com/pi6atv/winterhill-lib/internal/web/command-api"
@@ -17,20 +18,28 @@ import (
 
 var (
 	//go:embed dist
-	all embed.FS
+	all                   embed.FS
+	listenPort            = flag.String("listen", ":8080", "the address to listen on")
+	winterhillIP          = flag.String("winterhill-ip", "127.0.0.1", "winterhill IP")
+	winterhillCommandPort = flag.Int("winterhill-command-port", 9901, "winterhill command port")
+	winterhillBasePort    = flag.Int64("winterhill-base-port", 9920, "winterhill base port")
+	srResetDuration       = flag.Duration("symbol-rate-reset-duration", 30*time.Minute, "time after which the symbol rate is reset to default")
+	webPath               = flag.String("web-path", "/winterhill", "base path for the web pages and api")
+	//logEnabled            = flag.Bool()
 )
 
 func main() {
+	flag.Parse()
 	subdir, _ := fs.Sub(all, "dist")
 
 	logStream := log_stream.New()
 
-	statusApi, err := statusapi.New("127.0.0.1", 9901)
+	statusApi, err := statusapi.New(*winterhillIP, *winterhillCommandPort)
 	if err != nil {
 		panic(err)
 	}
 
-	commandApi, err := commandapi.New("127.0.0.1", 9920, 30*time.Minute, logStream)
+	commandApi, err := commandapi.New(*winterhillIP, *winterhillBasePort, *srResetDuration, logStream)
 	if err != nil {
 		panic(err)
 	}
@@ -39,14 +48,15 @@ func main() {
 	router.Use(middlewares.ExtractAuthMiddleware) // adds username from token to context
 
 	logApi := log_api.New(logStream)
-	router.Path("/winterhill/api/log/ws").HandlerFunc(logApi.WsHandler)
-	router.Path("/winterhill/api/status").HandlerFunc(statusApi.StatusHandler)
-	router.Path("/winterhill/api/summary").HandlerFunc(statusApi.SummaryHandler)
-	router.Path("/winterhill/api/config").HandlerFunc(statusApi.ConfigHandler)
-	router.Path("/winterhill/api/set/srate/{receiver:[1-4]}/{srate:[0-9]+}").HandlerFunc(commandApi.SetSymbolRateHandler).Methods("POST")
-	router.PathPrefix("/winterhill/").Handler(http.StripPrefix("/winterhill/", http.FileServer(http.FS(subdir))))
+	apiRouter := router.Path(*webPath)
+	apiRouter.Path("/api/log/ws").HandlerFunc(logApi.WsHandler)
+	apiRouter.Path("/api/status").HandlerFunc(statusApi.StatusHandler)
+	apiRouter.Path("/api/summary").HandlerFunc(statusApi.SummaryHandler)
+	apiRouter.Path("/api/config").HandlerFunc(statusApi.ConfigHandler)
+	apiRouter.Path("/api/set/srate/{receiver:[1-4]}/{srate:[0-9]+}").HandlerFunc(commandApi.SetSymbolRateHandler).Methods("POST")
+	apiRouter.PathPrefix("/").Handler(http.StripPrefix(*webPath+"/", http.FileServer(http.FS(subdir))))
 
 	router.Path("/metrics").Handler(promhttp.Handler())
 	fmt.Println("starting webserver")
-	_ = http.ListenAndServe(":8080", router)
+	_ = http.ListenAndServe(*listenPort, router)
 }
